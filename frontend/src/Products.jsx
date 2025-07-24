9f5abf72e217b1845764086d0930f8be2b0fcddb
Hayes O'Quinn <mhoquinn@icloud.com>
2025-07-24 18:45:47 -0500

    New products: 10 total

--- frontend/src/Products.jsx
+++ frontend/src/Products.jsx
@@ -6,6 +6,12 @@ export default function Products() {
     { id: 2, name: 'Product 2', img: '/productImages/steak.png' },
     { id: 3, name: 'Product 3', img: '/productImages/sushi.png' },
     { id: 4, name: 'Product 4', img: '/productImages/ramen.png' },
+    { id: 5, name: 'Product 5', img: '/productImages/burger.png' },
+    { id: 6, name: 'Product 6', img: '/productImages/pizza.png' },
+    { id: 7, name: 'Product 7', img: '/productImages/chicken.png' },
+    { id: 8, name: 'Product 8', img: '/productImages/lasagna.png' },
+    { id: 9, name: 'Product 9', img: '/productImages/salmon.png' },
+    { id: 10, name: 'Product 10', img: '/productImages/pie.png' },
   ];
 
   return (
